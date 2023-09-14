2113c8018f2c8535683af9e229fac7ef1411a262
dxrk-kali <dxrkanon@gmail.com>
2023-09-14 14:28:03 -0400

    deleted uness files

--- left.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "left" > /etc/hosts
-echo "left" > /etc/hostname
-echo "auto enp0s8" >> /etc/network/interfaces
-echo "iface enp0s8 inet static" >> /etc/network/interfaces
-echo "address 192.168.100.1" >> /etc/network/interfaces
-echo "netmask 255.255.255.0" >> /etc/network/interfaces
